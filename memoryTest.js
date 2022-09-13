98166cf7213eaeee154b339ae10e2153e21be4ea
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-09-13 18:34:15 +0200

    visualization

---
+++ memoryTest.js
@@ -0,0 +1,83 @@
+var cards = ["ciri.png", "geralt.png", "jaskier.png", "jaskier.png", "iorweth.png", "triss.png", 
+"geralt.png", "yen.png", "ciri.png", "triss.png", "yen.png", "iorweth.png"];
+
+//console.log(cards);
+
+var background = "url(img/karta.png)";
+var oneVisible = false;
+var turnCounter = 0;
+
+//c0.addEventListener("click", function() {revealCard(0);}); - 'normal way'
+//$(c0).on('click', function() {revealCard(0);}); - 'jQuery way'
+
+
+//turn ON fcn during startup
+window.onload = init;
+
+function init()
+{
+    for (var i=0; i<=11; i++)
+    {
+       (function(e){
+        $('#c'+e).click(function() {revealCard(e);});
+       })(i);
+    }
+}
+
+
+function revealCard(nr)
+{
+    var visibleNumber = 0;
+    var image = "url(img/" + cards[nr] +")";
+
+    $('#c'+nr).css('background-image', image);
+    /*
+    $('#c'+nr).addClass('cardA');
+    $('#c'+nr).removeClass('card');
+    */
+    $('#c'+nr).toggleClass('cardA'); //switch class
+
+    if (!oneVisible)
+    {
+        oneVisible = true;
+        visibleNumber = nr;
+    }
+    else
+    {
+        incrCount();
+        oneVisible = false;
+        compare(visibleNumber, nr);
+
+    }
+    
+
+}
+
+function incrCount()
+{
+    turnCounter++;
+    $('.score').html('Turn counter: '+turnCounter); //catching DIV by class
+}
+
+function compare(card1, card2)
+{
+    if (cards[card1] == cards[card2])
+        deletePair();
+    else
+        hideCards();
+}
+
+function hideCards(card1, card2)
+{
+    alert('pudlo');
+    $('#c'+card1).css('background-image', background);
+    $('#c'+card1).toggleClass('card');
+    $('#c'+card2).css('background-image', background);
+    $('#c'+card2).toggleClass('card');
+}
+
+function deletePair(card1, card2)
+{
+    alert('para');
+}
+
