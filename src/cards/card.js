19302dcb23a5ffb7b4a0ac855b06434b1edcd1b4
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2022-10-17 23:25:38 +0200

    RevealCard

--- src/cards/card.js
+++ src/cards/card.js
@@ -1,21 +1,40 @@
 import React, { useState } from 'react';
 import './card.css'
+import hogward from './img/hogwards.jpg'
+import harry from './img/harry.jpg'
+import ron from './img/ron.jpg'
+import hermione from './img/hermione.jpg'
+import dumpledore from './img/dumbledore.jpg'
+import voldemort from './img/voldemort.jpg'
+import dobby from './img/dobby.jpg'
 
 const Card = (props) => {
+    /* const IMG ={
+        school: require.context('./img/hogwards.jpg').default,
+    } */
+    const defaulfImage = './img/hogwards.jpg';
+    let image = props.character.split('.jpg');
+    //let visibleCardSide = defaulfImage;
+    let visibleCardSide = hogward;
 
-    var image = "url(img/" + props.character +")";
-
-    const [wasClicked, setCardClickedState] = useState(false);
+    const [wasClicked, setCardClickedState] = useState(true);
     //const [disable, setCardAsDisable] = useState(props.disable);
 
     const clickCardHandler = () => {
+        console.log(image[0]);
         setCardClickedState(!wasClicked);
         console.log('Updated!', wasClicked);
         props.clickedCard(wasClicked);
+        revealCard(wasClicked);
     }
 
+    const revealCard = (isActive) => {
+        isActive ? (visibleCardSide = image[0]) : (visibleCardSide = defaulfImage);
+        let styleee = {}
+    }
 
-    return <div className='card' onClick={clickCardHandler}></div>
+    return <div className='card' onClick={clickCardHandler} style={{backgroundImage : `url(${visibleCardSide})`}}></div>
+    //return <div className='card' onClick={clickCardHandler} style={{backgroundImage : `url(${IMG.school})`}}></div>
 }
 
 export default Card;
--- src/cards/cardBoard.js
+++ src/cards/cardBoard.js
@@ -5,33 +5,44 @@ import getRandomCardsOrder from './RandomCardsNum';
 
 
 const CardsBoard = (props) => {
+    const numberOfCards = props.items.length*2;
+    const characters = props.items;
+    let board = <p>There is not enough cards to display.</p>;
+    /* const defaulfImage = 'img/hogwards.jpg';
+    let image = 'img/' + props.character + '.jpg';
+    let visibleCardSide = defaulfImage; */
+    
 
      const clickedCardHandler = (reveivedData) => {
         const clicked = reveivedData;
         console.log('clicked!', clicked);
      };
 
-    
-     
-    const characters = props.items;
+    /*  const revealCard = (isActive) => {
+        isActive ? (visibleCardSide = image) : (visibleCardSide = defaulfImage);
+        console.log(visibleCardSide);
+    } */
 
-    const charactersSorted = getRandomCardsOrder(characters, 12);
-    for (let i=0; i<12; i++){
-        charactersSorted.splice(i, 1, {id: i, character: charactersSorted[i]});
-    }
+    if (numberOfCards != 0) {
+        const charactersSorted = getRandomCardsOrder(characters, numberOfCards);
 
-    return (<div className='board'>
-
-            {charactersSorted.map((element) => (
+        for (let i=0; i<numberOfCards; i++){
+            charactersSorted.splice(i, 1, {id: i, character: charactersSorted[i]});
+        }
+        
+        board = charactersSorted.map((element) => (
             <Card 
                 key={element.id} 
                 character={element.character} 
                 disable={true}
-                clickedCard={clickedCardHandler}>                
-            </Card>))}
+                clickedCard={clickedCardHandler}          
+            />))
+    }
 
-        <div className='score'>Turn counter: 0</div>
-    </div>)
+    return (<div className='board'>
+                {board}          
+                <div className='score'>Turn counter: 0</div>
+            </div>)
 }
 
 
